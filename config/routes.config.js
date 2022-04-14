4e4ff6484bd92196b3c53ca6f1d8c45e8a56ed63
Isa Gutierrez <isagr_kio@hotmail.com>
2022-04-14 02:05:26 +0200

    Fixed quiz & improved all questions.

--- config/routes.config.js
+++ config/routes.config.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const quizController = require('../controllers/quiz.controller')
 
 router.post('/new', quizController.create)
+router.get('/quiz', quizController.list)
 
 module.exports = router
--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -1,5 +1,11 @@
 const Question = require('../models/Question.model')
 
+module.exports.list = (req, res, next) => {
+    Question.find()
+        .then(questions => res.status(200).json(questions))
+        .catch(next)
+}
+
 module.exports.create = (req, res, next) => {
     const question = { title, solution, category } = req.body
     Question.create(question)
