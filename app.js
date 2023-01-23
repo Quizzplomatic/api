e3b755edab31767db9791a6e6feccc290c33a3b6
Isa Gutierrez <isagr_kio@hotmail.com>
2023-01-23 13:34:51 +0100

    Updated cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(logger('dev'));
 
 const corsOptions ={
-    origin: 'http://localhost:3000', 
+    origin: 'https://quizplomatic.netlify.app/', 
     credentials: true,
     optionSuccessStatus: 200
 }
